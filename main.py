83cc459fa014d8e6cafb4b1d8244b934d6caee6e
DavidVB10 <dvillablanco@gmail.com>
2024-03-22 20:52:03 +0100

    Minor changes

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+# ----- Inputs ----- #
 
-#INPUTS:
 """
 input_vel = input("Inserire un valore input per selezionare la velocità:\n "
 			    "0: c(x) = 1.3\n "
@@ -29,10 +29,13 @@ input_cond = input("Inserire un valore input per selezionare le condizioni sul b
 				   "2: Condizione Direchlet omogenea sul bordo inflow\n "
 """
 
-xmin, xmax, T = 0, 7, 5
-Nx = int(1e3)
+# ----- Funzioni e parametri della griglia ----- #
+
+xmin, xmax, T = -1, 3, 3
+Nx = int(5e2)
 x = np.linspace(xmin, xmax, Nx)
 dx = (xmax-xmin)/(Nx-1)
+print("Valore di dx: ",dx)
 
 def c(x, input):
 	if input == 0:
@@ -63,52 +66,78 @@ def u0(x, input, Nx = Nx):
 		u0 = np.sin(4*np.pi*x)*mask
 	return u0
 
-def UPWIND(U, c, Nx = Nx, Nt = Nt, lam = lam):
-	for n in range(Nt-1):
-		#U[:,n+1] = U[:,n]-c*lam*(U[:,n]-U[L,n])
-		U[:,n+1] = U[:,n]-c*lam*(U[R,n]-U[L,n])/2+abs(c)*lam*(U[R,n]-2*U[:,n]+U[L,n])/2
-	return U
-def LAX_FRIEDRICHS(U, c, Nx = Nx, Nt = Nt, lam = lam):
-	for n in range(Nt-1):
-		#U[:,n+1] = U[:,n]-c*lam*(U[:,n]-U[L,n])
-		U[:,n+1] = U[:,n]-c*lam*(U[R,n]-U[L,n])/2+abs(c)*lam*(U[R,n]-2*U[:,n]+U[L,n])/2
-	return U
-def LAX_WENDROFF(U, c, Nx = Nx, Nt = Nt, lam = lam):
-	for n in range(Nt-1):
-		#U[:,n+1] = U[:,n]-c*lam*(U[:,n]-U[L,n])
-		U[:,n+1] = U[:,n]-c*lam*(U[R,n]-U[L,n])/2+abs(c)*lam*(U[R,n]-2*U[:,n]+U[L,n])/2
-	return U
 
+def UPWIND(Um, c, n, Nx = Nx, Nt = Nt, lam = lam):
+    Up = Um[:]-c*lam*(Um[R]-Um[L])/2+abs(c)*lam*(Um[R]-2*Um[:]+Um[L])/2
+    if n==Nt-2:
+    	msg = "UPWIND"
+    	return Up, msg
+    return Up, None
 
-"""
-def LAX_FRIEDRICHS():
+def LAX_FRIEDRICHS(Um, c, n, Nx = Nx, Nt = Nt, lam = lam):
+	Up = (Um[R]+Um[L])/2-c*lam/2*(Um[R]-Um[L])
+	if n==Nt-2:
+		msg = "LAX FRIEDRICHS"
+		return Up, msg
+	return Up, None
 
-def LAX_WENDROFF():
-"""
+def LAX_WENDROFF(Um, c, n, Nx = Nx, Nt = Nt, lam = lam):
+	Up = Um[:]-c*lam/2*(Um[R]-Um[L])+c**2/2*lam**2*(Um[R]-2*Um[:]+Um[L])
+	if n==Nt-2:
+		msg = "LAX WENDROFF"
+		return Up, msg
+	return Up, None
+
+def condp(a, b, z, c):
+    l=b-a
+    if c>0:
+    	maskm = (z<a)
+    	z += l*maskm
+    if c<0:
+    	maskp = (z>b)
+    	z -= l*maskp
+    return z
 
 I = np.arange(Nx)
 L = I - 1
 R = I + 1
 # Condizioni periodiche
-#L[0] = Nx-1
-#R[-1] = 0
-# COndizioni al bordo inflow
+L[0] = Nx-1
+R[-1] = 0
+# Condizioni al bordo inflow
 
 
-
-U = np.zeros((Nx,Nt))
-start_time = time.time()
+U = np.zeros((Nx,Nt)) # Soluzione numerica
+Uex = np.zeros((Nx,Nt)) # Soluzione essata
 U[:,0] = u0(x, 0)
-U0 = U[:,0]
-end_time = time.time()
-print("Computing time for u0 calculation", end_time-start_time)
-print(U[:,0])
-U = UPWIND(U, c)
-print(U)
+Uex[:,0] = u0(x, 0)
+# Erorri
+err_l1 = np.zeros(Nt-1)
+err_linf = np.zeros(Nt-1)
 
+# ----- Ciclo principale ----- #
+
+start_time = time.time()
+for n in range(Nt-1):
+    U[:,n+1], msg = LAX_WENDROFF(U[:,n], c, n)
+    Uex[:,n+1] = u0(condp(xmin,xmax,x-c*n*dt,c),0)
+    U_l1 = np.linalg.norm(U[:,n+1],1)
+    U_linf = np.linalg.norm(U[:,n+1],np.inf)
+    Uex_l1 = np.linalg.norm(Uex[:,n+1],1)
+    Uex_linf = np.linalg.norm(Uex[:,n+1],np.inf)
+    err_l1[n] = abs((U_l1-Uex_l1)/Uex_l1)
+    err_linf[n] = abs((U_linf-Uex_linf)/Uex_linf)
+end_time = time.time()
+print("Tempo di computazione della U numerica e degli errori: ", end_time-start_time, "s")
+print("Errori metodo "+str(msg)+":\n"
+	  "	Errori norma l1: "+str(np.mean(err_l1))+"\n"
+	  "	Errori norma linf: "+str(np.mean(err_linf)))
+
+# ----- Plot ----- #
 
 plt.figure()
-line, = plt.plot(x, U0, "b-", label="Soluzione Numerica (UPWIND)")
+line, = plt.plot(x, U[:,0], "b.", label="Soluzione Numerica ("+str(msg)+")")
+line2, = plt.plot(x, Uex[:,0], "r-", label="Soluzione Essata")
 plt.legend()
 plt.title("Problemi iperbolici (Trasporto)")
 plt.xlim(xmin, xmax)
@@ -118,5 +147,6 @@ plt.ylabel("U")
 plt.legend()
 for n in range(Nt - 1):
     line.set_ydata(U[:,n+1])
+    line2.set_ydata(Uex[:,n+1])
     plt.pause(0.001)
 plt.show()
